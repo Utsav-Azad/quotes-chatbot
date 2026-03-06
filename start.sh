6d10e48ed40a65908e9682e8b89f53f1dc8db1b0
Utsav-Azad <utsav9c@gmail.com>
2026-03-06 13:34:57 +0530

    Add start script for Rasa + Flask

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
-rasa run --enable-api --cors "*" --port 5005 &
+#!/bin/sh
+
+echo "Starting Rasa Action Server..."
 rasa run actions &
-python app.py
+
+echo "Starting Rasa Server..."
+rasa run --enable-api --cors "*" --port 5005 &
+
+echo "Starting Flask Web App..."
+gunicorn app:app --bind 0.0.0.0:$PORT
