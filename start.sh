395d0a6fd8aea7374538bfde2fc7c0addcaf8f33
Utsav-Azad <utsav9c@gmail.com>
2026-03-06 18:29:35 +0530

    fix rasa startup delay

--- start.sh
+++ start.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-echo "Training Rasa model..."
-rasa train
-
 echo "Starting Action Server..."
 rasa run actions --port 5055 &
 
 echo "Starting Rasa Server..."
-rasa run --enable-api --cors "*" --port 5005 &
+rasa run --enable-api --cors "*" --port 5005 --model models &
 
-echo "Waiting for Rasa to start..."
 sleep 30
 
 echo "Starting Flask App..."
